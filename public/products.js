20cba6e7052fdf73cd0104474521f510b5e43605
t <yitingwu0719@gmail.com>
2025-10-08 14:36:04 +0800

    修改

---
+++ public/products.js
@@ -0,0 +1,25 @@
+// products.json 檔案內容範例
+
+[
+    { 
+        "id": 1, 
+        "zhTitle": "A", 
+        "enTitle": "A",
+        "category": "wood", 
+        "price": 1200,
+        "zhPrice": "NT$1200",
+        "enPrice": "USD$40",
+        "images": ["/uploads/teapot-1.jpg", "/uploads/teapot-2.jpg"]
+    },
+    { 
+        "id": 2, 
+        "zhTitle": "茶具組", 
+        "enTitle": "Tea Set",
+        "category": "tea_set", 
+        "price": 5000,
+        "zhPrice": "聯絡我們獲得報價",
+        "enPrice": "Contact us for a quote",
+        "images": ["/uploads/set-1.jpg"]
+    }
+    // ... 繼續添加您所有的商品物件 ...
+]
