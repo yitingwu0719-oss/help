fb5f18c4f8d8727e3947cf95a466e5a6328d5ddd
t <yitingwu0719@gmail.com>
2025-10-08 17:05:32 +0800

    a

---
+++ public/products.js
@@ -0,0 +1,22 @@
+[
+    { 
+        "id": 1, 
+        "zhTitle": "木雕茶壺 - 歲月痕跡", 
+        "enTitle": "Carved Teapot - Traces of Time",
+        "category": "wood", 
+        "price": 1200,
+        "zhPrice": "NT$1200",
+        "enPrice": "USD$40",
+        "images": ["/uploads/teapot-1.jpg", "/uploads/teapot-2.jpg"]
+    },
+    { 
+        "id": 2, 
+        "zhTitle": "茶具組", 
+        "enTitle": "Tea Set",
+        "category": "tea_set", 
+        "price": 5000,
+        "zhPrice": "聯絡我們獲得報價",
+        "enPrice": "Contact us for a quote",
+        "images": ["/uploads/set-1.jpg"]
+    }
+]
